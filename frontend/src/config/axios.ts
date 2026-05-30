ac6a8f0e2091fdb0f159e6a04543a2c31795925d
rehan2006ktm-source <rehan.2006ktm@gmail.com>
2026-05-30 20:04:52 +0530

    jj

--- frontend/src/config/axios.ts
+++ frontend/src/config/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api/v1',
+  baseURL: import.meta.env.VITE_API_URL || 'https://e-commerce-2-zpg7.onrender.com/api/v1',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
